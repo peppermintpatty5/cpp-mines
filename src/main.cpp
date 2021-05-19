5f50f6a67e0bee088c542be2a23b2bf9c1a762af
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-05-19 14:52:30 -0400

    Switch on enum better than unordered_map

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,50 @@
 #include <cstdlib>
 #include <iostream>
-#include <unordered_map>
 
 #include "mines.hpp"
 
+/**
+ * Maps each tile to a character representation
+ */
+static char tile_ch(tile t)
+{
+    switch (t)
+    {
+    case tile::PLAIN:
+        return '-';
+    case tile::ZERO:
+        return ' ';
+    case tile::ONE:
+        return '1';
+    case tile::TWO:
+        return '2';
+    case tile::THREE:
+        return '3';
+    case tile::FOUR:
+        return '4';
+    case tile::FIVE:
+        return '5';
+    case tile::SIX:
+        return '6';
+    case tile::SEVEN:
+        return '7';
+    case tile::EIGHT:
+        return '8';
+    case tile::MINE:
+        return '*';
+    case tile::DETONATED:
+        return '@';
+    case tile::FLAG_RIGHT:
+        return '#';
+    case tile::FLAG_WRONG:
+        return 'X';
+    default:
+        return '\0';
+    }
+}
+
 int main()
 {
-    static std::unordered_map<tile, char> const tile_ch = {
-        {tile::PLAIN, '-'},
-        {tile::ZERO, ' '},
-        {tile::ONE, '1'},
-        {tile::TWO, '2'},
-        {tile::THREE, '3'},
-        {tile::FOUR, '4'},
-        {tile::FIVE, '5'},
-        {tile::SIX, '6'},
-        {tile::SEVEN, '7'},
-        {tile::EIGHT, '8'},
-        {tile::MINE, '*'},
-        {tile::DETONATED, '@'},
-        {tile::FLAG_RIGHT, '#'},
-        {tile::FLAG_WRONG, 'X'},
-    };
-
     minesweeper game(0.17);
 
     for (cell_t::first_type x = 0; x < 10; x++)
@@ -33,7 +55,7 @@ int main()
     for (cell_t::first_type x = 0; x < 10; x++)
     {
         for (cell_t::second_type y = 0; y < 10; y++)
-            std::cout << tile_ch.at(game.get_tile({x, y})) << ' ';
+            std::cout << tile_ch(game.get_tile({x, y})) << ' ';
         std::cout << std::endl;
     }
 
