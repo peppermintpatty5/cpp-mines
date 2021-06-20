1fb94546ef43b680dd1c045455ebfeb5fab51e41
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-06-20 16:55:37 -0400

    Basic argument parsing for -d and -h

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
 #include <csignal>
+#include <cstdio>
 #include <cstdlib>
 
 #include "textui.hpp"
 
-int main()
+int main(int argc, char const *argv[])
 {
-    minesweeper game(0.17);
+    double density = 0.17;
+    minesweeper *game;
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            char flag = argv[i][1];
+
+            switch (flag)
+            {
+            case 'd':
+                density = std::atof(&argv[i][2]);
+                break;
+            case 'h':
+                std::printf("help\n");
+                std::exit(EXIT_SUCCESS);
+            default:
+                std::fprintf(stderr, "invalid option -- '%c'\n", flag);
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    game = new minesweeper(density);
 
     signal(SIGINT, SIG_IGN);
-    start_textui(game);
+    start_textui(*game);
 
     return EXIT_SUCCESS;
 }
